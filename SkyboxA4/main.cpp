053cdddacb5fd4b7f979601b1221a8cd4c89849c
SKYBOXLABS\akabak <alex.kabak@skyboxlabs.com>
2018-02-15 13:06:04 -0800

    static class for swapping not working; figure it out

--- SkyboxA4/main.cpp
+++ SkyboxA4/main.cpp
@@ -1,67 +1,14 @@
 #include <string>
 #include <iostream>
-
-void swap(int &a, int &b);
-void swap(bool &a, bool &b);
-void swap(std::string &a,  std::string &b);
+#include "swapper.h"
 
 int main()
 {
-	{
-		int a = 2;
-		int b = 4;
-
-		std::cout << "a: " << a << "; b: " << b << std::endl;
-		std::cout << "swap" << std::endl;
-		swap(a, b);
-		std::cout << "a: " << a << "; b: " << b << std::endl;
-		std::cout << std::endl;
-	}
-
-	{
-		bool ba = true;
-		bool bb = false;
-
-		std::cout << "bool a: " << ba << "; bool b: " << bb << std::endl;
-		std::cout << "swap using std::swap" << std::endl;
-		swap(ba, bb);
-		std::cout << "bool a: " << ba << "; bool b: " << bb << std::endl;
-		std::cout << std::endl;
-	}
-
-	{
-		std::string sa = "Hello";
-		std::string sb = "World";
-
-		std::cout << "string a: " << sa << "; string b: " << sb << std::endl;
-		std::cout << "swap using std::move" << std::endl;
-		swap(sa, sb);
-		std::cout << "string a: " << sa << "; string b: " << sb << std::endl;
-		std::cout << std::endl;
-	}
-
+	swapper::run();
 	system("pause");
 	return 0;
 }
 
-void swap(int &a, int &b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
-void swap(bool &a, bool &b)
-{
-	std::swap(a, b);
-}
-
-void swap(std::string &a, std::string &b)
-{
-	std::string temp = std::move(a);
-	a = std::move(b);
-	b = std::move(temp);
-}
 
 /*
 	1. int number = 10;
