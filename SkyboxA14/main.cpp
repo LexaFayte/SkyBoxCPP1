7c96d2f8de9c0db69f8aa8b5fcb2be4d26e237f3
Alexandra Kabak <luciferfayte@hotmail.com>
2018-04-05 20:19:11 -0700

    assignment 14
    assignment 14 complete as per specification.

--- SkyboxA14/main.cpp
+++ SkyboxA14/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <map>
+#include <functional>
+#include <stdexcept>
+#include <string>
 
 namespace MathUtil
 {
@@ -18,14 +21,13 @@ namespace MathUtil
 		return a * b;
 	}
 	float divide(float a, float b)
-	{
+	{	
 		if (b == 0)
 		{
-			throw "Error: Division by zero";			
+			throw std::overflow_error("Divide by zero exception");
 		}
 
 		return a / b;
-		
 	}
 }
 
@@ -33,6 +35,49 @@ int main()
 {
 	using namespace MathUtil;
 	
+	using arithmeticFunction = std::function<float(float, float)>;
+	std::map<char, arithmeticFunction> functions = {
+		std::pair<char,arithmeticFunction>('+',add),
+		std::pair<char,arithmeticFunction>('-',subtract),
+		std::pair<char,arithmeticFunction>('*',multiply),
+		std::pair<char,arithmeticFunction>('/',divide)
+	};
+
+	float lhs = 0;
+	float rhs = 0;
+	char operation = ' ';
+	
+	std::cout << "please enter the first number of the arithmetic expresion to calculate: ";
+	std::cin >> lhs;
+	std::cout << "please enter the second number of the arithmetic expression to calculate: ";
+	std::cin >> rhs;
+	std::cout << "please enter the arithmetic operation you want to execute (+, -, *, /)";
+	std::cin >> operation;
+
+	float result = 0;
+	switch (operation)
+	{
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+		try
+		{
+			result = functions[operation](lhs, rhs);
+		}
+		catch (std::overflow_error e)
+		{
+			std::cout << e.what() << std::endl;
+			std::cout << "exiting the program" << std::endl;
+			system("pause");
+			return 0;
+		}
+		break;
+	}
+	
+	std::cout << std::endl;
+	std::cout << lhs << " " << operation << " " << rhs << " = " << result << std::endl;
+
 	system("pause");
 	return 0;
 }
