b9b9aaa33a3d82de776065d8c174c3b03fc8448a
Alexandra Kabak <luciferfayte@hotmail.com>
2018-05-06 11:13:17 -0700

    Made some changes to incorporate a ticketing/handle system

--- SkyboxA20/Button.h
+++ SkyboxA20/Button.h
@@ -1,24 +1,49 @@
 #pragma once
 #include <vector>
+#include <map>
 #include <functional>
+
+struct CallBackFunction
+{
+
+private:
+	int mTicket = 0;
+public:
+	std::function<void()> mFunction;
+
+	CallBackFunction(std::function<void()>& fn, int ticket)
+	{
+		mFunction = fn;
+		mTicket = ticket;
+	}
+
+	bool operator==(CallBackFunction& rhs)
+	{
+		if (mTicket == rhs.mTicket)
+		{
+			return true;
+		}
+		return false;
+	}
+};
 
 namespace _ButtonCallBack
 {
 	struct CallBack
 	{
 	private:
-		std::vector<std::function<void()>> mRegisteredFunctions;
+		std::vector<CallBackFunction> mRegisteredFunctions;
 	public:
-		void operator+=(std::function<void()>& rhs)
+		void operator+=(CallBackFunction rhs)
 		{
 			mRegisteredFunctions.push_back(rhs);
 		}
 
-		void operator-=(std::function<void()>& rhs)
+		void operator-=(CallBackFunction rhs)
 		{
 			for (auto it = mRegisteredFunctions.begin(); it != mRegisteredFunctions.end(); ++it)
 			{
-				if (it->target_type() == rhs.target_type())
+				if (*it == rhs)
 				{
 					mRegisteredFunctions.erase(it);
 					break;
@@ -33,17 +58,17 @@ namespace _ButtonCallBack
 
 class Button
 {
-private:
-
-	
-
+private:	
+	int mTicketCounter = 0;
 public:
 	_ButtonCallBack::CallBack PressCallBack;
 	_ButtonCallBack::CallBack ReleaseCallBack;
 
 	void Press();
 	void Release();	
-
-	/*void Register(std::function<void()>& fn);
-	void Unregister(std::function<void()>& fn);*/
+	CallBackFunction CreateCallBackFunction(std::function<void()>& fn)
+	{
+		CallBackFunction function(std::forward<decltype(fn)>(fn), mTicketCounter++);
+		return function;
+	}
 };
